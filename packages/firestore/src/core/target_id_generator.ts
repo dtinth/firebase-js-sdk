c55430b0fc893b4f1e23ad5816f5d2a989c08179
Sebastian Schmidt <mrschmidt@google.com>
2018-06-12 17:54:40 -0700

    [AUTOMATED]: Prettier Code Styling

--- packages/firestore/src/core/target_id_generator.ts
+++ packages/firestore/src/core/target_id_generator.ts
@@ -71,7 +71,7 @@ export class TargetIdGenerator {
   }
 
   /** Verifies that the provided ID lies within this generators ID space. */
-  covers(targetId: TargetId) : boolean {
+  covers(targetId: TargetId): boolean {
     return (targetId & RESERVED_BITS) === this.generatorId;
   }
 
